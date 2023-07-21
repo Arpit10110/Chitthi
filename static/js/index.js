67d69da6211ed4b7c56df963c7aa006fc1488159
Arpit <omagrahari55@gmail.com>
2023-07-21 15:33:39 +0530

    second commit

--- static/js/index.js
+++ static/js/index.js
@@ -2,16 +2,22 @@ const socket = io()
 let Uname;
 let textarea = document.querySelector('#textarea')
 let messageArea = document.querySelector('.msgarea')
+var messC="";
 do {
     Uname = prompt('Please enter your name: ')
 } while(!Uname)
 
 textarea.addEventListener('keyup', (e) => {
     if(e.key === 'Enter') {
-        sendMessage(e.target.value)
+        if(e.target.value=='\n')
+        {
+            alert("Please enter the message");
+        }
+        else{
+            sendMessage(e.target.value)
+        }
     }
 })
-
 function sendMessage(message) {
     let msg = {
         user: Uname,
@@ -21,15 +27,22 @@ function sendMessage(message) {
     appendMessage(msg, 'incoming')
     textarea.value = ''
     scrollToBottom()
-
     // Send to server 
     socket.emit('message', msg)
-
 }
 
 function appendMessage(msg, type) {
     let mainDiv = document.createElement('div')
     let className = type
+    if(type=='incoming') 
+    {
+        const audio = new Audio("./music/sent.mp3");
+        audio.play(); 
+    }
+    else{
+        const audio = new Audio("./music/rece.mp3");
+    audio.play(); 
+    }
     mainDiv.classList.add(className, 'message')
     let markup = `
         <h4>${msg.user}</h4>
@@ -48,5 +61,3 @@ socket.on('message', (msg) => {
 function scrollToBottom() {
     messageArea.scrollTop = messageArea.scrollHeight
 }
-
-
